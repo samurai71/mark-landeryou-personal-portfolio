02bd215dea2dbd6b8a11b7491d5d4d4275b393ff
Mark Landeryou <samurai96@hotmail.com>
2022-10-05 23:59:09 -0400

    added skills and their logos to skills component

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -9,17 +9,24 @@ import Github from '../public/assets/skills/github1.png';
 import Firebase from '../public/assets/skills/firebase.png';
 import NextJS from '../public/assets/skills/nextjs.png';
 import NodeJS from '../public/assets/skills/node.png';
+import MongoDB from '../public/assets/skills/mongo.png';
+import ViteJS from '../public/assets/skills/vite-svgrepo-com.svg';
+import VercelJS from '../public/assets/skills/vercel.svg';
+import WebpackJS from '../public/assets/skills/webpack-svgrepo-com.svg';
 import AWS from '../public/assets/skills/aws.png';
 
 const Skills = () => {
   return (
     <section id='skills' className='w-full p-2 lg:h-screen'>
       <div className='mx-auto flex h-full max-w-[1240px] flex-col justify-center'>
-        <p className='text-xl uppercase tracking-widest text-blue-700'>
-          Skills
-        </p>
-        <h2 className='py-4'>What I Can Do</h2>
-        <h3 className='py-4'>Front-End Skills</h3>
+        <div>
+          <p className='text-xl uppercase tracking-widest text-blue-700'>
+            Skills
+          </p>
+          <h2 className='py-4'>What I Can Do</h2>
+        </div>
+
+        {/* <h3 className='py-4'>Front-End Skills</h3> */}
         <div className='grid grid-cols-2 gap-8 lg:grid-cols-4'>
           <div className='rounded-xl p-6 shadow-xl duration-300 ease-in hover:scale-105'>
             <div className='grid grid-cols-2 items-center justify-center gap-4'>
@@ -111,16 +118,14 @@ const Skills = () => {
               </div>
             </div>
           </div>
-        </div>
-        <h3 className='py-4'>Back-End Skills</h3>
-        <div className='grid grid-cols-2 gap-8 lg:grid-cols-4'>
+
           <div className='rounded-xl p-6 shadow-xl duration-300 ease-in hover:scale-105'>
             <div className='grid grid-cols-2 items-center justify-center gap-4'>
               <div className='m-auto'>
-                <Image src={Javascript} width='64px' height='64px' alt='/' />
+                <Image src={MongoDB} width='64px' height='64px' alt='/' />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>JavaScript</h3>
+                <h3>MongoDB</h3>
               </div>
             </div>
           </div>
@@ -128,20 +133,10 @@ const Skills = () => {
           <div className='rounded-xl p-6 shadow-xl duration-300 ease-in hover:scale-105'>
             <div className='grid grid-cols-2 items-center justify-center gap-4'>
               <div className='m-auto'>
-                <Image src={Firebase} width='64px' height='64px' alt='/' />
+                <Image src={ViteJS} width='64px' height='64px' alt='/' />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>Firebase</h3>
-              </div>
-            </div>
-          </div>
-          <div className='rounded-xl p-6 shadow-xl duration-300 ease-in hover:scale-105'>
-            <div className='grid grid-cols-2 items-center justify-center gap-4'>
-              <div className='m-auto'>
-                <Image src={Github} width='64px' height='64px' alt='/' />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>Github</h3>
+                <h3>ViteJS</h3>
               </div>
             </div>
           </div>
@@ -149,14 +144,27 @@ const Skills = () => {
           <div className='rounded-xl p-6 shadow-xl duration-300 ease-in hover:scale-105'>
             <div className='grid grid-cols-2 items-center justify-center gap-4'>
               <div className='m-auto'>
-                <Image src={NodeJS} width='64px' height='64px' alt='/' />
+                <Image src={VercelJS} width='64px' height='64px' alt='/' />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>Node</h3>
+                <h3>VercelJS</h3>
+              </div>
+            </div>
+          </div>
+          <div className='rounded-xl p-6 shadow-xl duration-300 ease-in hover:scale-105'>
+            <div className='grid grid-cols-2 items-center justify-center gap-4'>
+              <div className='m-auto'>
+                <Image src={WebpackJS} width='64px' height='64px' alt='/' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <h3>WebpackJS</h3>
               </div>
             </div>
           </div>
         </div>
+
+        {/* <h3 className='py-4'>Back-End Skills</h3> */}
+        <div className='grid grid-cols-2 gap-8 lg:grid-cols-4'></div>
       </div>
     </section>
   );
