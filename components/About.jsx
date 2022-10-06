bbf252c0bc243e710485a801da70d4ffb158bb9e
Mark Landeryou <samurai96@hotmail.com>
2022-10-05 20:40:30 -0400

    added a profile picture of myself changed the link colour to red

--- components/About.jsx
+++ components/About.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import AboutImg from '../public/assets/about.jpg';
+import AboutImg from '../public/assets/Mark_blackshirt.jpg';
 
 const About = () => {
   return (
-    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
-      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+    <section
+      id='about'
+      className='flex w-full items-center p-2 py-16 md:h-screen'
+    >
+      <div className='m-auto max-w-[1240px] grid-cols-3 gap-8 md:grid'>
         <div className='col-span-2'>
-          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
+          <p className='text-xl uppercase tracking-widest text-[#5651e5]'>
             About
           </p>
           <h2 className='py-4'>Who I Am</h2>
@@ -32,16 +35,16 @@ const About = () => {
             learning new technologies.
           </p>
           <Link href='/#projects'>
-            <p className='py-2 text-gray-600 underline cursor-pointer'>
+            <p className='cursor-pointer py-2 text-red-600 underline'>
               Check out some of my latest projects.
             </p>
           </Link>
         </div>
-        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+        <div className='m-auto flex h-auto w-full items-center justify-center rounded-xl p-4 shadow-xl shadow-gray-400 duration-300 ease-in hover:scale-105'>
           <Image src={AboutImg} className='rounded-xl' alt='/' />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
