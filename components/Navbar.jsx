7f4372ae1ee2b40ab777e6b44fdb05b1bb3a4ebc
Mark Landeryou <samurai96@hotmail.com>
2022-10-05 20:04:40 -0400

    fixed the navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 // import { useRouter } from 'next/router';
-import NavLogo from '../public/assets/navLogo.png'
+//import NavLogo from '../public/assets/navLogo.png'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -50,12 +50,12 @@ const Navbar = () => {
       style={{ backgroundColor: `${navBg}` }}
       className={
         shadow
-          ? 'fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300'
-          : 'fixed w-full h-20 z-[100]'
+          ? 'fixed z-[100] h-20 w-full shadow-xl duration-300 ease-in-out'
+          : 'fixed z-[100] h-20 w-full'
       }
     >
-      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-        <Link href='/'>
+      <div className='flex h-full w-full items-center justify-center px-2 2xl:px-16'>
+        {/* <Link href='/'>
           <a>
             <Image
               src={NavLogo}
@@ -65,7 +65,7 @@ const Navbar = () => {
               className='cursor-pointer'
             />
           </a>
-        </Link>
+        </Link> */}
         <div>
           <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
             <li className='ml-10 text-sm uppercase hover:border-b'>
@@ -102,43 +102,38 @@ const Navbar = () => {
       {/* Overlay */}
       <div
         className={
-          nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''
+          nav ? 'fixed left-0 top-0 h-screen w-full bg-black/70 md:hidden' : ''
         }
       >
         {/* Side Drawer Menu */}
         <div
           className={
             nav
-              ? ' fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
-              : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+              ? ' fixed left-0 top-0 h-screen w-[75%] bg-[#ecf0f3] p-10 duration-500 ease-in sm:w-[60%] md:w-[45%]'
+              : 'fixed left-[-100%] top-0 p-10 duration-500 ease-in'
           }
         >
           <div>
             <div className='flex w-full items-center justify-between'>
-              <Link href='/'>
+              {/* <Link href='/'>
                 <a>
-                  <Image
-                    src={NavLogo}
-                    width='87'
-                    height='35'
-                    alt='/'
-                  />
+                  <Image src={NavLogo} width='87' height='35' alt='/' />
                 </a>
-              </Link>
+              </Link> */}
               <div
                 onClick={handleNav}
-                className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'
+                className='cursor-pointer rounded-full p-3 shadow-lg shadow-gray-400'
               >
                 <AiOutlineClose />
               </div>
             </div>
-            <div className='border-b border-gray-300 my-4'>
-              <p className='w-[85%] md:w-[90%] py-4'>
+            <div className='my-4 border-b border-gray-300'>
+              <p className='w-[85%] py-4 md:w-[90%]'>
                 Let&#39;s build something legendary together
               </p>
             </div>
           </div>
-          <div className='py-4 flex flex-col'>
+          <div className='flex flex-col py-4'>
             <ul className='uppercase'>
               <Link href='/'>
                 <li onClick={() => setNav(false)} className='py-4 text-sm'>
@@ -175,13 +170,13 @@ const Navbar = () => {
               <p className='uppercase tracking-widest text-[#5651e5]'>
                 Let&#39;s Connect
               </p>
-              <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+              <div className='my-4 flex w-full items-center justify-between sm:w-[80%]'>
                 <a
                   href='https://www.linkedin.com/in/clint-briley-50056920a/'
                   target='_blank'
                   rel='noreferrer'
                 >
-                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                  <div className='cursor-pointer rounded-full p-3 shadow-lg shadow-gray-400 duration-300 ease-in hover:scale-105'>
                     <FaLinkedinIn />
                   </div>
                 </a>
@@ -190,14 +185,14 @@ const Navbar = () => {
                   target='_blank'
                   rel='noreferrer'
                 >
-                  <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                  <div className='cursor-pointer rounded-full p-3 shadow-lg shadow-gray-400 duration-300 ease-in hover:scale-105'>
                     <FaGithub />
                   </div>
                 </a>
                 <Link href='/#contact'>
                   <div
                     onClick={() => setNav(!nav)}
-                    className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                    className='cursor-pointer rounded-full p-3 shadow-lg shadow-gray-400 duration-300 ease-in hover:scale-105'
                   >
                     <AiOutlineMail />
                   </div>
@@ -205,7 +200,7 @@ const Navbar = () => {
                 <Link href='/resume'>
                   <div
                     onClick={() => setNav(!nav)}
-                    className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
+                    className='cursor-pointer rounded-full p-3 shadow-lg shadow-gray-400 duration-300 ease-in hover:scale-105'
                   >
                     <BsFillPersonLinesFill />
                   </div>
