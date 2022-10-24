78540c75d16b7722724f21c92c11243c1cedea3b
Mark Landeryou <samurai96@hotmail.com>
2022-10-24 11:40:08 -0400

    changed the colour of the link in the project files created a component folder and some files for better maintenance

--- pages/crypto.jsx
+++ pages/crypto.jsx
@@ -7,22 +7,22 @@ import Link from 'next/link';
 const crypto = () => {
   return (
     <div className='w-full'>
-      <div className='w-screen h-[50vh] relative'>
-        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+      <div className='relative h-[50vh] w-screen'>
+        <div className='absolute top-0 left-0 z-10 h-[50vh] w-full bg-black/70' />
         <Image
-          className='absolute z-1'
+          className='z-1 absolute'
           layout='fill'
           objectFit='cover'
           src={cryptoImg}
           alt='/'
         />
-        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+        <div className='absolute top-[70%] left-[50%] right-[50%] z-10 w-full max-w-[1240px] translate-x-[-50%] translate-y-[-50%] p-2 text-white'>
           <h2 className='py-2'>Crypto App</h2>
           <h3>React JS / Tailwind / Firebase</h3>
         </div>
       </div>
 
-      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
+      <div className='mx-auto grid max-w-[1240px] gap-8 p-2 py-8 md:grid-cols-5'>
         <div className='col-span-4'>
           <p>Project</p>
           <h2>Overview</h2>
@@ -32,54 +32,54 @@ const crypto = () => {
             hosted on Firebase. This application supports user authentication
             using firebase. Users are able to create a new account using their
             email address and can then sign in to save coins to a user specific
-            list. This is made possble by creating a firestore cloud storage
+            list. This is made possible by creating a firestore cloud storage
             database at the time of user registration. Another feature of this
-            application is dynamic routing through the &quot;React Router DOM&quot;
-            package. This application was built using the Coin Gecko API.
+            application is dynamic routing through the &quot;React Router
+            DOM&quot; package. This application was built using the Coin Gecko
+            API.
           </p>
           <a
             href='https://github.com/fireclint/crypto-react-firebase'
             target='_blank'
             rel='noreferrer'
           >
-            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+            <button className='mt-4 mr-8 px-8 py-2'>Code</button>
           </a>
           <a
             href='https://cryptobase-yt.web.app/'
             target='_blank'
             rel='noreferrer'
           >
-            <button className='px-8 py-2 mt-4'>Demo</button>
+            <button className='mt-4 px-8 py-2'>Demo</button>
           </a>
-
         </div>
-        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+        <div className='col-span-4 rounded-xl py-4 shadow-xl shadow-gray-400 md:col-span-1'>
           <div className='p-2'>
-            <p className='text-center font-bold pb-2'>Technologies</p>
+            <p className='pb-2 text-center font-bold'>Technologies</p>
             <div className='grid grid-cols-3 md:grid-cols-1 '>
-              <p className='text-gray-600 py-2 flex items-center'>
+              <p className='flex items-center py-2 text-gray-600'>
                 <RiRadioButtonFill className='pr-1' /> React
               </p>
-              <p className='text-gray-600 py-2 flex items-center'>
+              <p className='flex items-center py-2 text-gray-600'>
                 <RiRadioButtonFill className='pr-1' /> Tailwind
               </p>
-              <p className='text-gray-600 py-2 flex items-center'>
+              <p className='flex items-center py-2 text-gray-600'>
                 <RiRadioButtonFill className='pr-1' /> Javascript
               </p>
-              <p className='text-gray-600 py-2 flex items-center'>
+              <p className='flex items-center py-2 text-gray-600'>
                 <RiRadioButtonFill className='pr-1' /> Firebase
               </p>
-              <p className='text-gray-600 py-2 flex items-center'>
+              <p className='flex items-center py-2 text-gray-600'>
                 <RiRadioButtonFill className='pr-1' /> Coin Gecko API
               </p>
-              <p className='text-gray-600 py-2 flex items-center'>
+              <p className='flex items-center py-2 text-gray-600'>
                 <RiRadioButtonFill className='pr-1' /> Routes
               </p>
             </div>
           </div>
         </div>
         <Link href='/#projects'>
-          <p className='underline cursor-pointer'>Back</p>
+          <p className='cursor-pointer text-red-600 underline'>Back</p>
         </Link>
       </div>
     </div>
